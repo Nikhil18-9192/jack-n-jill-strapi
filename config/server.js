2945bd18a6efe99d3b9f69f1bb30923c06df2ff5
Rohan Lamb <lambrohan@gmail.com>
2021-06-07 11:16:45 +0530

    init

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '0b3db777d09464de032ee9b375e261b3'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '1eccde24-9d51-42dd-85eb-d2a39f566a29'
+};
