a62e0a22d77916b555942e9124b4ae18b25f317b
nikhil18-9192 <nikhilkorane08@gmail.com>
2021-06-07 14:10:43 +0530

    create product api

---
+++ api/product/controllers/product.js
@@ -0,0 +1,19 @@
+'use strict';
+
+/**
+ * Read the documentation (https://strapi.io/documentation/developer-docs/latest/development/backend-customization.html#core-controllers)
+ * to customize this controller
+ */
+
+module.exports = {
+    async create(ctx) {
+        ctx.badRequest("Bad Request");
+    },
+
+    async update(ctx) {
+        ctx.badRequest("Bad Request");
+    },
+    async delete(ctx) {
+        ctx.badRequest("Bad Request");
+    },
+};
