0de823921002802b040b37efe253049ddcf89a3c
Rohan Lamb <lambrohan@gmail.com>
2021-06-16 14:59:46 +0530

    prod

--- config/database.js
+++ config/database.js
@@ -4,18 +4,12 @@ module.exports = ({ env }) => ({
     default: {
       connector: "bookshelf",
       settings: {
-        client: "postgres",
-        host: env("QOVERY_DATABASE_PSQL_HOST", "localhost"),
-        port: env.int("DATABASE_PORT", 5432),
-        database: env("QOVERY_DATABASE_PSQL_DATABASE", "strapi"),
-        username: env("QOVERY_DATABASE_PSQL_USERNAME", "strapi"),
-        password: env("QOVERY_DATABASE_PSQL_PASSWORD", "strapi"),
-        schema: env("DATABASE_SCHEMA", "public"), // Not Required
-        ssl: {
-          rejectUnauthorized: env.bool("DATABASE_SSL_SELF", false), // For self-signed certificates
-        },
+        client: "sqlite",
+        filename: env("DATABASE_FILENAME", ".tmp/data.db"),
       },
-      options: {},
+      options: {
+        useNullAsDefault: true,
+      },
     },
   },
 });
